c40f654ec1157dd82154f7862aa170d49a216533
ernesto diaz <ernesto.diaz@miliopay.com>
2024-02-11 19:52:11 -0500

    :sparkles: configure patter for catch message from topic

--- src/producer/producer.controller.ts
+++ src/producer/producer.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import ProducerService from './producer.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('producer')
 export default class ProducerController {
   constructor(private readonly producerService: ProducerService) {}
+
+  @MessagePattern('YAPE_TRANSACTIONS')
+  public catchMessage(@Payload() payload: any) {
+    Logger.log(payload, ProducerController.name);
+  }
 }
