13e71c8acb71bbc68fb19cd2b6e338ae67554e3b
ernesto diaz <ernesto.diaz@miliopay.com>
2024-02-11 20:30:11 -0500

    :sparkles: configure client for consume from topic

--- src/producer/producer.controller.ts
+++ src/producer/producer.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Logger } from '@nestjs/common';
+import { Body, Controller, Inject, Logger, Post } from '@nestjs/common';
 import ProducerService from './producer.service';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { ClientProxy, MessagePattern, Payload } from '@nestjs/microservices';
+
+const TOPIC = 'YAPE_TRANSACTIONS';
 
 @Controller('producer')
 export default class ProducerController {
-  constructor(private readonly producerService: ProducerService) {}
+  constructor(
+    private readonly producerService: ProducerService,
+    @Inject('KAFKA') private readonly kafka: ClientProxy,
+  ) {}
 
-  @MessagePattern('YAPE_TRANSACTIONS')
+  @MessagePattern(`${TOPIC}`)
   public catchMessage(@Payload() payload: any) {
     Logger.log(payload, ProducerController.name);
+  }
+
+  @Post('send')
+  public sendMessage(
+    @Body('message') message: string,
+    @Body('user') user: string,
+  ) {
+    Logger.log(TOPIC, '<====');
+    return this.kafka.emit(TOPIC, { message, user });
   }
 }
