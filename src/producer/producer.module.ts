94a3f8a91bc20c83ee985f86ffcdd093cb14a0d9
ernesto diaz <ernesto.diaz@miliopay.com>
2024-02-11 20:30:03 -0500

    :sparkles: configure client for consume from kafka

--- src/producer/producer.module.ts
+++ src/producer/producer.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import ProducerService from './producer.service';
 import ProducerController from './producer.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { Partitioners } from 'kafkajs';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'billing',
+            brokers: ['localhost:9092'],
+          },
+          producer: {
+            createPartitioner: Partitioners.DefaultPartitioner,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [ProducerController],
   providers: [ProducerService],
 })
